7f2d615cc77b810bee098a3420083680b042f242
Ashwin Singh <91330777+ashwinsingh87@users.noreply.github.com>
2023-04-07 22:40:28 +0530

    second

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-const head1 = React.createElement("h1", { id: "header1" }, "hi i am header1");
-console.log(head1);
-const head2 = React.createElement("h1", {}, "hi i am header2");
-const container = React.createElement("div", {}, [head1, head2]);
+import React from "react";
+import  ReactDOM  from "react-dom/client";
+
+const head1 = React.createElement("h1", { id: "header1" }, "hi i am header1 from react");
+// console.log(head1);
+const head2 = React.createElement("h1", {id:"head2"}, "hi i am header2");
+const container = React.createElement("div", {id:"container", className:"random", hello:"world" } , [head1, head2,]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(container);
 
-// jsx _ html+js
-const head = <h1>helo</h1>
-create react-app my-app
+// const head = <h1>helo</h1>
+// create react-app my-app
---
+++ readme.txt
@@ -0,0 +1,19 @@
+application production ready=> bundler
+bundler{minify, bundle, caching, chunking}=> babel
+babel=> jsx>js readable  form understandable browser.
+react>jsx
+jsx> js+html
+default react> webpack=> bundler
+npm :- node package manager
+npm init 
+npm i -D parcel
+npm i react
+npm i react-dom
+cdn-content delivery network
+application ignite=> npx parcel index.html
+npx => np- node module, x-execute
+bundler - parcel
+entry point- index.html
+script source> type="module"
+import React from react
+import ReactDom from react-dom/client
